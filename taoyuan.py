6d1ba6abb83dd5950b650925ed255e2028eaba8f
Dark Tornado <dt3141592@naver.com>
2026-03-18 01:22:46 +0900

    타오위안 공항 첩운 완성

--- taoyuan.py
+++ taoyuan.py
@@ -21,7 +21,7 @@ class Taoyuan:
         }
         uds = {
             'N': 'up',
-            'S': 'down'
+            'S': 'dn'
         }
 
         data = []
@@ -43,5 +43,23 @@ class Taoyuan:
                 'ud': uds[datum['direction']]
             })
 
-        return data
-        
+        result = []
+        stns = [{'s': '타이베이역 (台北車站)', 'id': 'A01'}, {'s': '싼충 (三重)', 'id': 'A02'}, {'s': '신베이산업단지 (新北產業園區)', 'id': 'A03'}, {'s': '신좡부도심 (新莊副都心)', 'id': 'A04'}, {'s': '타이산 (泰山)', 'id': 'A05'}, {'s': '타이산구이허 (泰山貴和)', 'id': 'A06'}, {'s': '국립타이완 체육대학 (體育大學)', 'id': 'A07'}, {'s': '창겅병원 (長庚醫院)', 'id': 'A08'}, {'s': '린커우 (林口)', 'id': 'A09'}, {'s': '산비 (山鼻)', 'id': 'A10'}, {'s': '컹커우 (坑口)', 'id': 'A11'}, {'s': '공항 터미널 1 (機場第一航廈)', 'id': 'A12'}, {'s': '공항 터미널 2 (機場第二航廈)', 'id': 'A13'}, {'s': '공항호텔 (機場旅館)', 'id': 'A14a'}, {'s': '다위안 (大園)', 'id': 'A15'}, {'s': '헝산 (橫山)', 'id': 'A16'}, {'s': '링항 (領航)', 'id': 'A17'}, {'s': '타오위안 고속철도역 (高鐵桃園站)', 'id': 'A18'}, {'s': '타오위안 체육공원 (桃園體育園區)', 'id': 'A19'}, {'s': '싱난 (興南)', 'id': 'A20'}, {'s': '환베이 (環北)', 'id': 'A21'}, {'s': '라오제시 (老街溪)', 'id': 'A22'}]
+        for i in range(0, len(stns)):
+            stn = stns[i]
+            result.append({
+                'stn': stn['s'],
+                'up': [],
+                'dn': []
+            })
+
+            for train in data:
+                if train['stn'] == stns[i]['id']:
+                    result[i][train['ud']].append({
+                        'no': train['no'],
+                        'type': train['type'],
+                        'status': train['status'],
+                        'dest': train['dest']
+                    })
+
+        return result
